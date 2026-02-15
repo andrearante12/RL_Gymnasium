c73ceab630f994d949040d82bbedba9d171d332d
IrvineZou <wenchengz@email.gwu.edu>
2026-02-15 10:38:25 -0500

    Update xxx.py

--- CarRacing-v3/xxx.py
+++ CarRacing-v3/xxx.py
@@ -6,11 +6,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import PPO_Net,Critic_Net,CNN_Net
-#CarRaceAgent
-#Single agent
-#reward: -0.1 for each step without touching any track tile
-#+1000/N, if touched a track tile in this track, N 
-#is the total number of tiles in this track
 import gymnasium as gym
 import numpy as np
 from collections import defaultdict,deque
@@ -19,3 +14,22 @@ from typing import Optional
 import random
 from dataclasses import dataclass
 import cv2
+
+def make_env(render_mode=None):
+    """Create and wrap CarRacing environment"""
+    env = gym.make("CarRacing-v3", continuous=True, render_mode=render_mode,domain_randomize=True)
+    #Your wrapper is here:
+    
+    return env
+
+#Your agent is here
+class xxxAgent(nn.Module):
+    def __init__(self):
+
+    def act(self, x):
+
+    def load_parameter(self,file):
+
+    #other functions can be extended here
+
+      
